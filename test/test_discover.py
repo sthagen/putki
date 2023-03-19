56fe8364c1e459c2574579060d18622f59526501
Stefan Hagen <stefan@hagen.link>
2023-03-19 22:18:16 +0100

    Adjusted tests

--- test/test_discover.py
+++ test/test_discover.py
@@ -2,11 +2,11 @@ import putki.discover as discover
 
 
 def test_root_default():
-    assert discover.root() == ''
+    assert discover.root().endswith('example/basic/tasks')
 
 
 def test_root_cwd_is_root():
-    assert discover.root('.') == ''
+    assert discover.root('.').endswith('example/basic/tasks')
 
 
 def test_root_within_not_existing():
