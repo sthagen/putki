5fad370ab3c719aede96aeb03678cef82da288da
Stefan Hagen <stefan@hagen.link>
2023-03-21 00:59:40 +0100

    Added transformer for tasks_seq to actions

--- putki/discover.py
+++ putki/discover.py
@@ -42,12 +42,39 @@ def tasks(below: str | pathlib.Path = '.') -> dict[str, list[dict[str, str]]]:
 @no_type_check
 def combine(jobs: dict[str, list[dict[str, str]]]) -> list[dict[str, str]]:
     """Combine the tasks by mapping the local ids to path prefixed ids."""
-    tasks: list[dict[str, str]] = []
+    tasks_seq: list[dict[str, str]] = []
     common = min(sorted(jobs.keys()))
     for path, job_seq in jobs.items():
         prefix = path.replace(common, '', 1)
         for task in job_seq:
             local_id = task['id']
             task['id'] = f'{prefix}/{local_id}'
-            tasks.append(task)
-    return tasks
+            tasks_seq.append(task)
+    return tasks_seq
+
+
+@no_type_check
+def assemble_path(path_elements: dict[str, str]) -> str:
+    """Assemble a connection string from /source/path_elements."""
+    address_template = path_elements['address_template']
+    return (
+        address_template.replace('{{protocol}}', path_elements['protocol'])
+        .replace('{{host}}', path_elements['host'])
+        .replace('{{port}}', path_elements['port'])
+        .replace('{{service_root}}', path_elements['service_root'])
+    )
+
+
+@no_type_check
+def derive(tasks_seq: list[dict[str, str | dict[str, str]]]) -> dict[str, dict[str, str | int | dict[str, str]]]:
+    """Derive map with actionable names by mapping the path prefixed ids and assembling path elements."""
+    actions: dict[str, dict[str, str | int | dict[str, str]]] = {}
+    for slot, task in enumerate(tasks_seq):
+        folder_slug = task['id'].replace('/', '_')
+
+        action: dict[str, str | int] = {**task, 'rank': slot}
+        source: dict[str, str | dict[str, str]] = task['source']
+        action['url'] = source['path'] if source.get('path') else assemble_path(source['path_elements'])
+        actions[folder_slug] = action
+
+    return actions
